59bbaf8995721061853deb84d3391101165a8680
Melbinex <58853396+Melbinex@users.noreply.github.com>
2021-11-17 20:54:45 +0100

    change lib folder

---
+++ Eleonora-sorokina/js/burger.js
@@ -0,0 +1,11 @@
+$(function() {
+    $(".hamburger").click(function() {
+    $(".hamburger").toggleClass("is-active");
+        })
+    });
+
+
+
+    $( ".hamburger" ).click(function() {
+        $( ".selector" ).slideToggle('fast');
+      });
