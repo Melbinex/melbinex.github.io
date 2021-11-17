f26c53208a5910d16b147da36a6db625381d36a8
Melbinex <58853396+Melbinex@users.noreply.github.com>
2021-11-17 13:57:50 +0100

    add hamburger for phone

--- Eleonora-sorokina/js/script.js
+++ Eleonora-sorokina/js/script.js
@@ -1,3 +1,18 @@
 $('.arrow').click(function(){
     $('html').animate({scrollTop : 0},800);
    });
+
+
+
+
+   $(function() {
+    $(".hamburger").click(function() {
+    $(this).toggleClass("is-active");
+        })
+    });
+
+
+
+    $( ".hamburger" ).click(function() {
+        $( ".selector" ).slideToggle();
+      });
