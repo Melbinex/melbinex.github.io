4d7aa9c51f410756a17c29de22c834395710eae7
dmykolaenko0255@gmail.com <Danchik_1233212133>
2022-03-20 10:21:40 +0200

    rework render DOM

--- Elia/js/script.js
+++ Elia/js/script.js
@@ -4,3 +4,38 @@ $('#najnowsze-naglowki').vTicker('init', {
   showItems: 1,
   padding: 1
 });
+
+const gallery = document.querySelector('#gallery')
+
+const galleryItems = [
+  {
+    title: 'Familio',
+    subTitle: 'App concept, Behance',
+  },
+  {
+    title: 'WePay',
+    subTitle: 'Shopping app, Behance',
+  },
+  {
+    title: 'AnyKey',
+    subTitle: 'E-commerce site (Coming soon)',
+  },
+  {
+    title: 'Smart home',
+    subTitle: 'Shots, Behance',
+  },
+  {
+    title: 'S.G. museum',
+    subTitle: 'Landing page, Tilda',
+  }
+]
+console.log('galleryItems');
+galleryItems.map((item, index) => {
+
+  gallery.innerHTML += `
+      <div class="gallery_${index+1} gallery_all">
+          <p class="gallery_tittle">${item.title}</p>
+          <p class="gallery__${index+1}_description gallery_description_all">${item.subTitle}</p>
+      </div>
+    `
+})
